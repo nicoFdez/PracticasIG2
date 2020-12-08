4674119f6d3c1cd00b6c1cbff8cbdf24dc4909d0
Javier Cruz López de Ochoa <javcruz@ucm.es>
2020-12-08 18:52:33 +0100

    2.end()--

--- Practica2/ProyectosOGREvc15x86/media/IG2App/EsferaAgujerosFS.glsl
+++ Practica2/ProyectosOGREvc15x86/media/IG2App/EsferaAgujerosFS.glsl
@@ -1,22 +1,56 @@
 #version 330 core
-uniform sampler2D texturaCorrosion; // tipo sampler2D para texturas 2D
-uniform float intLuzAmb; // luz ambiente blanca
 in vec2 vUv0; // out del vertex shader
+in vec2 vUv1;
+in vec3 vVertex;
+in vec3 vNormal;
+
+uniform sampler2D texturaCorrosion; // tipo sampler2D para texturas 2D
+uniform sampler2D texturaBumpy;
+uniform float intLuzAmb; // luz ambiente blanca
 uniform vec4 InColor;
 uniform vec4 OutColor;
+
+uniform vec3 lightAmbient; // intensidades de la luz
+uniform vec3 lightDiffuse;
+uniform vec4 lightPosition; // datos de la fuente de luz en view space
+uniform vec3 materialDiffuse; // datos del material ¡Front=Back!
+
 out vec4 fFragColor; // out del fragment shader
+
+float diff(vec3 cVertex, vec3 cNormal)
+{
+    vec3 lightDir = lightPosition.xyz; // directional light ?
+    if (lightPosition.w == 1) { 
+        lightDir = lightPosition.xyz - cVertex;
+    }
+    return max(dot(cNormal, normalize(lightDir)), 0.0);
+    // dot: coseno ángulo
+}
 
 void main() {
     vec3 colorCorrosion = vec3(texture(texturaCorrosion, vUv0)); // acceso a téxel
     vec4 color;
-    if(colorCorrosion.r < 0.5){
+    if(colorCorrosion.r > 0.6){
         discard;
     }
     else{
-        if (gl_FrontFacing) color = OutColor;
-        else color = InColor;
+        // ambient
+        vec3 ambient = lightAmbient * materialDiffuse;
+
+        // diffuse en view space
+        vec3 diffuse;
+        if (gl_FrontFacing){
+            diffuse = diff(vVertex, vNormal) * lightDiffuse * materialDiffuse;
+            color = vec4((ambient + diffuse), 1.0) * OutColor; // + specular
+
+            vec4 colorBumpy = vec4(vec3(texture(texturaBumpy, vUv0)), 1.0); 
+            color = color * colorBumpy;
+        } 
+        else {
+            diffuse = diff(vVertex, -vNormal) * lightDiffuse * materialDiffuse;
+            color = vec4((ambient + diffuse), 1.0) * InColor; // + specular
+        }
     }
+
     fFragColor = color; //* intLuzAmb // out
-    // mix ‐> (1‐BF).colorL + BF.colorM
-    
 }
--- Practica2/ProyectosOGREvc15x86/media/IG2App/EsferaAgujerosVS.glsl
+++ Practica2/ProyectosOGREvc15x86/media/IG2App/EsferaAgujerosVS.glsl
@@ -1,12 +1,24 @@
 #version 330 core
 in vec4 vertex; // atributos de los vértices a procesar
+in vec3 normal;
 in vec2 uv0; // coordenadas de textura 0
+in vec2 uv1;
+
+uniform mat4 modelViewMat;
 uniform mat4 modelViewProjMat; // constante de programa
+uniform mat4 normalMat; 
+
 out vec2 vUv0; // out del vertex shader
+out vec2 vUv1;
+out vec4 vVertex;
+out vec3 vNormal;
 
 void main() {
     // se pasan las coordenadas de textura
     vUv0 = uv0;
+    vUv1 = uv1;
     gl_Position = modelViewProjMat * vertex; //obligatorio
     // (Clipping coordinates)
+    vVertex = modelViewMat * vertex;
+    vNormal = normalize(vec3(normalMat * vec4(normal,0)));
 }
